7e1bc1862e2cd0db703374125a544009b3e08086
bassel <bassel.alazab@gmail.com>
2020-08-16 04:50:21 +0200

    Add required annotations to deal with beans and for search classes, and activate MVC module.

--- src/main/java/com/basola/pcapp/config/SpringWebConfig.java
+++ src/main/java/com/basola/pcapp/config/SpringWebConfig.java
@@ -1,6 +1,13 @@
 
 package com.basola.pcapp.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = {"com.basola"})
+@EnableWebMvc
 
 public class SpringWebConfig {
     
