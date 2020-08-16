5a967cc9685d292a73fcf08d96b89a49bf0b5dfe
bassel <bassel.alazab@gmail.com>
2020-08-16 05:40:06 +0200

    Adding request for specific url (for test it), and created function that return name of jsp file.

--- src/main/java/com/basola/pcapp/controller/TestController.java
+++ src/main/java/com/basola/pcapp/controller/TestController.java
@@ -1,6 +1,12 @@
 
 package com.basola.pcapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class TestController {
     
+    @RequestMapping("/test/hello")
+    public String helloWorld (){
+        return "hello";
+    }
 }
