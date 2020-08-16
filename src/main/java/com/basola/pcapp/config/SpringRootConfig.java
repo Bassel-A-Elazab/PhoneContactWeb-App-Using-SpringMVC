eba452360acbed2e420522acddbf3a1eda97f812
bassel <bassel.alazab@gmail.com>
2020-08-16 04:47:37 +0200

    configure beans in spring container, @Configuration to obtain bean definition methods, @ComponentScan to scan for classes in packages.

--- src/main/java/com/basola/pcapp/config/SpringRootConfig.java
+++ src/main/java/com/basola/pcapp/config/SpringRootConfig.java
@@ -1,7 +1,15 @@
 
 package com.basola.pcapp.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.basola"})
+
 
 public class SpringRootConfig {
+    
+     //TODO later for services and repository
     
 }
