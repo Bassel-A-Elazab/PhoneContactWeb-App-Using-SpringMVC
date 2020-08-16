882d9408b88d87b1c14ce0606d6b70abecfbc0ec
bassel <bassel.alazab@gmail.com>
2020-08-16 06:04:13 +0200

    define variables that holds user data and getter & setter method to deal with it.

--- src/main/java/com/basola/pcapp/domain/User.java
+++ src/main/java/com/basola/pcapp/domain/User.java
@@ -1,6 +1,88 @@
-
 package com.basola.pcapp.domain;
 
 public class User {
+
+    private Integer userID;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private String loginName;
+    private String password;
+    private Integer role;
+    private Integer loginStatus;
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    public Integer getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(Integer loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+    
     
 }
