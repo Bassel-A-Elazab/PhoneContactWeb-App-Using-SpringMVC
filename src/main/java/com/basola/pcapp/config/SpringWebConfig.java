022c263c07f376a2ef2ed27400fa251c51aadb7e
bassel <bassel.alazab@gmail.com>
2020-08-16 05:35:22 +0200

    I'm not remember to extends spring mvc configure using WebMvcConfigureAdapter and add annotations Bean to viewResolver function

--- src/main/java/com/basola/pcapp/config/SpringWebConfig.java
+++ src/main/java/com/basola/pcapp/config/SpringWebConfig.java
@@ -1,10 +1,12 @@
-
 package com.basola.pcapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -12,16 +14,16 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan(basePackages = {"com.basola"})
 @EnableWebMvc
 
-public class SpringWebConfig {
-    
-    
-        public ViewResolver viewResolver(){
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver() {
         InternalResourceViewResolver vr = new InternalResourceViewResolver();
         vr.setViewClass(JstlView.class);
         vr.setPrefix("/WEB-INF/view/");
         vr.setSuffix(".jsp");
-        
+
         return vr;
     }
-        
+
 }
