62b41b5867826d808c437142f120b5f4c0023f7a
bassel <bassel.alazab@gmail.com>
2020-08-17 15:26:32 +0200

    test query by get specific record by ID.

--- src/test/java/com/basola/pcapp/test/TestUserDAOQuery.java
+++ src/test/java/com/basola/pcapp/test/TestUserDAOQuery.java
@@ -1,6 +1,30 @@
-
 package com.basola.pcapp.test;
 
+import com.basola.pcapp.config.SpringRootConfig;
+import com.basola.pcapp.dao.UserDAO;
+import com.basola.pcapp.domain.User;
+import javax.faces.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class TestUserDAOQuery {
-    
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringRootConfig.class);
+        UserDAO userDAO = ctx.getBean(UserDAO.class);
+        
+        // Find Single Record
+        User u = userDAO.findById(3);
+        System.out.println("User Info : ");
+        System.out.println(u.getUserID());
+        System.out.println(u.getName());
+        System.out.println(u.getPhone());
+        System.out.println(u.getEmail());
+        System.out.println(u.getAddress());
+        System.out.println(u.getLoginName());
+        System.out.println(u.getLoginStatus());
+        System.out.println(u.getRole());
+
+    }
 }
