9cdf3140e111aafc0d7c7cc880491549b4e3f165
bassel <bassel.alazab@gmail.com>
2020-08-17 18:24:51 +0200

    define methods of contact services.

--- src/main/java/com/basola/pcapp/service/ContactService.java
+++ src/main/java/com/basola/pcapp/service/ContactService.java
@@ -1,7 +1,24 @@
 
 package com.basola.pcapp.service;
 
+import com.basola.pcapp.domain.Contact;
+import java.util.List;
 
-public class ContactService {
+
+public interface ContactService {
+    
+    public void save(Contact c);
+    
+    public void update(Contact c);
+    
+    public void delete(Integer contactId);
+    
+    public void delete(Integer[] contactId);
+    
+    public Contact findById(Integer contactId);
+    
+    public List<Contact> findUserContact(Integer userId);
+    
+    public List<Contact> findUserContact(Integer userId, String txt);
     
 }
