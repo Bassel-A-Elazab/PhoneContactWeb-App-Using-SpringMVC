b0231a935417642f39e42c51b8cd023bdca67b4f
bassel <bassel.alazab@gmail.com>
2020-08-17 17:24:48 +0200

    this method handle login operation(authentication), and will thrown by userBlockedException if user account is blocked.

--- src/main/java/com/basola/pcapp/service/UserService.java
+++ src/main/java/com/basola/pcapp/service/UserService.java
@@ -2,11 +2,14 @@
 package com.basola.pcapp.service;
 
 import com.basola.pcapp.domain.User;
+import com.basola.pcapp.exception.UserBlockedException;
 
 
 public interface UserService {
     
     public void register(User u);
+    
+    public User login(String loginName, String password) throws UserBlockedException;
     
     
 }
