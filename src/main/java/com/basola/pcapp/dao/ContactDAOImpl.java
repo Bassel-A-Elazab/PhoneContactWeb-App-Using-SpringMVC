c38cd20fb38aded0e6f7939cf66a09a36ddb17f9
bassel <bassel.alazab@gmail.com>
2020-08-17 15:43:58 +0200

    insert to mysql database using NamedParamter in spring framework

--- src/main/java/com/basola/pcapp/dao/ContactDAOImpl.java
+++ src/main/java/com/basola/pcapp/dao/ContactDAOImpl.java
@@ -1,6 +1,63 @@
-
 package com.basola.pcapp.dao;
 
-public class ContactDAOImpl {
-    
+import com.basola.pcapp.domain.Contact;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+public class ContactDAOImpl extends BaseDAO implements ContactDAO {
+
+    @Override
+    public void save(Contact c) {
+        
+        String sql = "INSERT INTO contact(userId, name, phone, email, address, remark) VALUES(:userId, :name, :phone, :email, :address, :remark)";
+        Map m = new HashMap();
+        m.put("userId", c.getUserID());
+        m.put("name", c.getName());
+        m.put("phone", c.getPhone());
+        m.put("email", c.getEmail());
+        m.put("address", c.getAddress());
+        m.put("remark", c.getRemark());
+        SqlParameterSource ps = new MapSqlParameterSource(m);
+        KeyHolder kh = new GeneratedKeyHolder();
+        getNamedParameterJdbcTemplate().update(sql, ps, kh);
+        c.setContactID(kh.getKey().intValue());
+        
+    }
+
+    @Override
+    public void update(Contact u) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Contact u) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer contactId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Contact findById(Integer contactId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Contact> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Contact> findByProperty(String propName, Object propValue) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
