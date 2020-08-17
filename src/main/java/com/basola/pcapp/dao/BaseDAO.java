7533b37d53bf5b5d348ffc65bb67b4c092b4b37a
bassel <bassel.alazab@gmail.com>
2020-08-17 07:53:10 +0200

    to deal with data source using spring with UserDAO and Contact DAO.

--- src/main/java/com/basola/pcapp/dao/BaseDAO.java
+++ src/main/java/com/basola/pcapp/dao/BaseDAO.java
@@ -1,7 +1,17 @@
 
 package com.basola.pcapp.dao;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 
-public class BaseDAO {
+
+public class BaseDAO extends NamedParameterJdbcDaoSupport {
     
+    @Autowired
+    public void setDataSource2(DataSource ds){
+    
+        super.setDataSource(ds);
+        
+    }
 }
