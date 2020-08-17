94a753fa52eae60c45ec6001cd50e35364ad67ec
bassel <bassel.alazab@gmail.com>
2020-08-17 07:47:34 +0200

    Created methods that's applied CRUD operation for contacts.

--- src/main/java/com/basola/pcapp/dao/ContactDAO.java
+++ src/main/java/com/basola/pcapp/dao/ContactDAO.java
@@ -1,7 +1,22 @@
-
 package com.basola.pcapp.dao;
 
+import com.basola.pcapp.domain.Contact;
+import java.util.List;
 
-public class ContactDAO {
-    
+public interface ContactDAO {
+
+    public void save(Contact u);
+
+    public void update(Contact u);
+
+    public void delete(Contact u);
+
+    public void delete(Integer contactId);
+
+    public Contact findById(Integer contactId);
+
+    public List<Contact> findAll();
+
+    public List<Contact> findByProperty(String propName, Object propValue);
+
 }
