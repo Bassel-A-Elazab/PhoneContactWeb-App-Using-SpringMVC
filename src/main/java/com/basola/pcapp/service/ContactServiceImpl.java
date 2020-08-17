2a52595ad21e57dd0ad0a1e5f3b475186b1686eb
bassel <bassel.alazab@gmail.com>
2020-08-17 18:43:00 +0200

    delete collection of contacts and using my static method that's convert contactID to separate ID commas.

--- src/main/java/com/basola/pcapp/service/ContactServiceImpl.java
+++ src/main/java/com/basola/pcapp/service/ContactServiceImpl.java
@@ -3,6 +3,7 @@ package com.basola.pcapp.service;
 import com.basola.pcapp.dao.BaseDAO;
 import com.basola.pcapp.dao.ContactDAO;
 import com.basola.pcapp.domain.Contact;
+import com.basola.pcapp.util.StringUtil;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,7 +31,9 @@ public class ContactServiceImpl extends BaseDAO implements ContactService {
     
     @Override
     public void delete(Integer[] contactId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String ids = StringUtil.toCommaSeparatedString(contactId);
+        String sql = "DELETE FROM contact WHERE contactID IN("+ids+")";
+        getJdbcTemplate().update(sql);
     }
     
     @Override
