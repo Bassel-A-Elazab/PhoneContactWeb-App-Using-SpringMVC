87df3c495ceb6bcfe7a230de392818c1f082062e
bassel <bassel.alazab@gmail.com>
2020-08-17 18:51:23 +0200

    customized this method to get contact by using search free text.

--- src/main/java/com/basola/pcapp/service/ContactServiceImpl.java
+++ src/main/java/com/basola/pcapp/service/ContactServiceImpl.java
@@ -3,6 +3,7 @@ package com.basola.pcapp.service;
 import com.basola.pcapp.dao.BaseDAO;
 import com.basola.pcapp.dao.ContactDAO;
 import com.basola.pcapp.domain.Contact;
+import com.basola.pcapp.rm.ContactRowMapper;
 import com.basola.pcapp.util.StringUtil;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,45 +11,46 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ContactServiceImpl extends BaseDAO implements ContactService {
-    
+
     @Autowired
     private ContactDAO contactDAO;
-    
+
     @Override
     public void save(Contact c) {
         contactDAO.save(c);
     }
-    
+
     @Override
     public void update(Contact c) {
         contactDAO.update(c);
     }
-    
+
     @Override
     public void delete(Integer contactId) {
         contactDAO.delete(contactId);
     }
-    
+
     @Override
     public void delete(Integer[] contactId) {
         String ids = StringUtil.toCommaSeparatedString(contactId);
-        String sql = "DELETE FROM contact WHERE contactID IN("+ids+")";
+        String sql = "DELETE FROM contact WHERE contactID IN(" + ids + ")";
         getJdbcTemplate().update(sql);
     }
-    
+
     @Override
     public Contact findById(Integer contactId) {
-       return contactDAO.findById(contactId);
+        return contactDAO.findById(contactId);
     }
-    
+
     @Override
     public List<Contact> findUserContact(Integer userId) {
         return contactDAO.findByProperty("userId", userId);
     }
-    
+
     @Override
     public List<Contact> findUserContact(Integer userId, String txt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT contactId, userId, name, phone, email, address, remark FROM contact WHERE userId=? AND (name LIKE '%" + txt + "%' OR address LIKE '%" + txt + "%' OR phone LIKE '%" + txt + "%' OR email LIKE '%" + txt + "%' OR remark LIKE '%" + txt + "%')";
+        return getJdbcTemplate().query(sql, new ContactRowMapper(), userId);
     }
-    
+
 }
