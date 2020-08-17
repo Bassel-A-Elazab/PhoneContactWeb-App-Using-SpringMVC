a81542b5b28f4e8cb7a8b890d7c29b6b14e93fd1
bassel <bassel.alazab@gmail.com>
2020-08-17 08:09:52 +0200

    define all absrtact method and do operaations in save, 1 - insert to sql by using named paramter.

--- src/main/java/com/basola/pcapp/dao/UserDAOImpl.java
+++ src/main/java/com/basola/pcapp/dao/UserDAOImpl.java
@@ -1,7 +1,70 @@
-
 package com.basola.pcapp.dao;
 
+import com.basola.pcapp.domain.User;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
-public class UserDAOImpl {
-    
+@Repository
+public class UserDAOImpl extends BaseDAO implements UserDAO {
+
+    @Override
+    public void save(User u) {
+        String sql = "INSERT INTO user(name, phone, email, address, loginName, password, role, loginStatus)"
+                + "VALUES(:name, :phone, :email, :address, :loginName, :password, :role, :loginStatus)";
+        Map m = new HashMap();
+        m.put("name", u.getName());
+        m.put("phone", u.getPhone());
+        m.put("email", u.getEmail());
+        m.put("address", u.getAddress());
+        m.put("loginName", u.getLoginName());
+        m.put("password", u.getPassword());
+        m.put("role", u.getRole());
+        m.put("loginStatus", u.getLoginStatus());
+        
+        KeyHolder kh = new GeneratedKeyHolder();       //for hold id autoincremented by mysql
+        SqlParameterSource ps = new MapSqlParameterSource(m);
+        super.getNamedParameterJdbcTemplate().update(sql, ps, kh);
+        Integer userId = kh.getKey().intValue();
+        u.setUserID(userId);
+    }
+
+    @Override
+    public void update(User u) {
+
+    }
+
+    @Override
+    public void delete(User u) {
+
+    }
+
+    @Override
+    public void delete(Integer userId) {
+
+    }
+
+    @Override
+    public User findById(Integer userId) {
+        return null;
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+
+    }
+
+    @Override
+    public List<User> findByProperty(String propName, Object propValue) {
+        return null;
+
+    }
+
 }
