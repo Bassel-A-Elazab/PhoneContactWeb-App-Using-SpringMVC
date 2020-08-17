fe1ed8d1be58939d65ff0e82485cadf8b0293027
bassel <bassel.alazab@gmail.com>
2020-08-17 08:31:35 +0200

    add one user to test save ,ethod in UserDAO.

--- src/main/java/com/basola/pcapp/config/SpringRootConfig.java
+++ src/main/java/com/basola/pcapp/config/SpringRootConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan(basePackages = {"com.basola"})
+@ComponentScan(basePackages = {"com.basola.pcapp.dao"})
 
 
 public class SpringRootConfig {
--- src/main/java/com/basola/pcapp/dao/TestUserDAOSave.java
+++ src/main/java/com/basola/pcapp/dao/TestUserDAOSave.java
@@ -1,11 +1,27 @@
-
 package com.basola.pcapp.dao;
+
+import com.basola.pcapp.config.SpringRootConfig;
+import com.basola.pcapp.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestUserDAOSave {
 
     public static void main(String[] args) {
-        
-        
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringRootConfig.class);
+        UserDAO userDAO = ctx.getBean(UserDAO.class);
+        User u = new User();
+        u.setName("Usama");
+        u.setPhone("0123652314");
+        u.setEmail("usama.serag@gmail.com");
+        u.setAddress("alex");
+        u.setLoginName("usama");
+        u.setPassword("456789");
+        u.setRole(1);               //Admin Role 
+        u.setLoginStatus(1);         //Active
+        userDAO.save(u);
+        System.out.println("--------Data Saved------");
     }
-    
+
 }
