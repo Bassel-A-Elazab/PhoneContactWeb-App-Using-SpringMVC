8f0b2e2dc0642273bb55629f6bbb0c2a7989f5b4
bassel <bassel.alazab@gmail.com>
2020-08-17 15:46:59 +0200

    update query of sql using NamedParamter with spring framework.

--- src/main/java/com/basola/pcapp/dao/ContactDAOImpl.java
+++ src/main/java/com/basola/pcapp/dao/ContactDAOImpl.java
@@ -14,7 +14,7 @@ public class ContactDAOImpl extends BaseDAO implements ContactDAO {
 
     @Override
     public void save(Contact c) {
-        
+
         String sql = "INSERT INTO contact(userID, name, phone, email, address, remark) VALUES(:userId, :name, :phone, :email, :address, :remark)";
         Map m = new HashMap();
         m.put("userId", c.getUserID());
@@ -27,12 +27,22 @@ public class ContactDAOImpl extends BaseDAO implements ContactDAO {
         KeyHolder kh = new GeneratedKeyHolder();
         getNamedParameterJdbcTemplate().update(sql, ps, kh);
         c.setContactID(kh.getKey().intValue());
-        
+
     }
 
     @Override
-    public void update(Contact u) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void update(Contact c) {
+
+        String sql = "UPDATE contact SET name=:name, phone=:phone, email=:email, address=:address, remark=:remark WHERE contactID=:contactID";
+        Map m = new HashMap();
+        m.put("contactId", c.getContactID());
+        m.put("name", c.getName());
+        m.put("phone", c.getPhone());
+        m.put("email", c.getEmail());
+        m.put("address", c.getAddress());
+        m.put("remark", c.getRemark());
+        getNamedParameterJdbcTemplate().update(sql, m);
+
     }
 
     @Override
