e1c4b3b93b579d21ffa18d13ee9eaabac2e472fe
bassel <bassel.alazab@gmail.com>
2020-08-17 05:25:18 +0200

    get datasource with conguration pool of jdbc and test one statement sql.

--- src/test/java/com/basola/pcapp/test/TestDataSource.java
+++ src/test/java/com/basola/pcapp/test/TestDataSource.java
@@ -1,11 +1,24 @@
 
 package com.basola.pcapp.test;
 
+import javax.sql.DataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 public class TestDataSource {
 
     
     public static void main(String[] args) {
+        
+        ApplicationContext ctx = new AnnotationConfigApplicationContext();
+        DataSource ds = ctx.getBean(DataSource.class);
+        JdbcTemplate jt = new JdbcTemplate(ds);
+        String sql = "INSERT INTO USER ('name', 'phone', 'email', 'address', 'loginName', 'password') VALUES (?,?,?,?,?,?)";
+        Object[] param = new Object[]{"Bassel,01094630032","bassel.alazab@gmail.com","cairo","basola","123456"};
+        jt.update(sql,param);
+        System.out.println("SQL Executed Success...");
         
         
     }
