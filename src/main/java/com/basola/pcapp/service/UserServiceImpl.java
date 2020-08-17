2a506ef2cc1868f70882e4a7c1f815f9f10959f5
bassel <bassel.alazab@gmail.com>
2020-08-17 17:46:39 +0200

    handle register method by calls save method that inherit from UserDAO.

--- src/main/java/com/basola/pcapp/service/UserServiceImpl.java
+++ src/main/java/com/basola/pcapp/service/UserServiceImpl.java
@@ -1,6 +1,45 @@
 
 package com.basola.pcapp.service;
 
-public class UserServiceImpl {
+import com.basola.pcapp.dao.BaseDAO;
+import com.basola.pcapp.dao.UserDAO;
+import com.basola.pcapp.domain.User;
+import com.basola.pcapp.exception.UserBlockedException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl extends BaseDAO implements UserService{
+
+    @Autowired
+    private UserDAO userDAO;
+    
+    @Override
+    public void register(User u) {
+        
+        userDAO.save(u);
+        
+    }
+
+    @Override
+    public User login(String loginName, String password) throws UserBlockedException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<User> getuserList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void changeLoginStatus(Integer userId, Integer loginStatus) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Boolean isUserNameExist(String username) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
