22cf324d85e452d757225381c5f71a06884b41a2
bassel <bassel.alazab@gmail.com>
2020-08-17 19:54:07 +0200

    creates login tasks to user.

--- src/main/java/com/basola/pcapp/service/UserServiceImpl.java
+++ src/main/java/com/basola/pcapp/service/UserServiceImpl.java
@@ -5,8 +5,12 @@ import com.basola.pcapp.dao.BaseDAO;
 import com.basola.pcapp.dao.UserDAO;
 import com.basola.pcapp.domain.User;
 import com.basola.pcapp.exception.UserBlockedException;
+import com.basola.pcapp.rm.UserRowMapper;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,7 +28,22 @@ public class UserServiceImpl extends BaseDAO implements UserService{
 
     @Override
     public User login(String loginName, String password) throws UserBlockedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT userID, name, phone,email, address, role, loginStatus,loginName"
+                +" FROM user WHERE loginName=:ln AND password=:pw";
+        
+        Map m = new HashMap();
+        m.put("ln",loginName);
+        m.put("pw",password);
+        try{
+            User u = getNamedParameterJdbcTemplate().queryForObject(sql, m, new UserRowMapper());
+            if(u.getLoginStatus().equals(UserService.LOGIN_STATUS_BLOCKED)){
+                throw new UserBlockedException("Your account has been blocked. Contact to admin");
+            }else{
+                return u;
+            }
+        }catch(EmptyResultDataAccessException ex){
+            return null;
+        }
     }
 
     @Override
