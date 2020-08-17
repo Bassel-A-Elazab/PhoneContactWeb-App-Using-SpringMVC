7afbf00bfa13450420f660f901a59154a34e21d2
bassel <bassel.alazab@gmail.com>
2020-08-17 17:39:03 +0200

    define constant variables that belong to status and role of users.

--- src/main/java/com/basola/pcapp/service/UserService.java
+++ src/main/java/com/basola/pcapp/service/UserService.java
@@ -8,6 +8,12 @@ import java.util.List;
 
 public interface UserService {
     
+    public static final Integer LOGIN_SATUS_ACTIVE = 1;
+    public static final Integer LOGIN_STATUS_BLOCKED = 2;
+    
+    public static final Integer ROLE_ADMIN = 1;
+    public static final Integer ROLE_USER = 2;
+    
     public void register(User u);
     
     public User login(String loginName, String password) throws UserBlockedException;
