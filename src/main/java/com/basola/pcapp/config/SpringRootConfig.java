644a9f3a5932dd94c994e400dbfc44bc173822b3
bassel <bassel.alazab@gmail.com>
2020-08-17 05:15:31 +0200

    Configuration database using connection pool to test it.

--- src/main/java/com/basola/pcapp/config/SpringRootConfig.java
+++ src/main/java/com/basola/pcapp/config/SpringRootConfig.java
@@ -1,6 +1,8 @@
 
 package com.basola.pcapp.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,4 +14,19 @@ public class SpringRootConfig {
     
      //TODO later for services and repository
     
+    @Bean
+    public BasicDataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/contact_phone");
+        ds.setUsername("root");
+        ds.setPassword("root");
+        ds.setMaxTotal(2);
+        ds.setInitialSize(1);
+        ds.setTestOnBorrow(true);
+        ds.setValidationQuery("SELECT 1");
+        ds.setDefaultAutoCommit(true);
+        
+        return ds;
+    }
 }
