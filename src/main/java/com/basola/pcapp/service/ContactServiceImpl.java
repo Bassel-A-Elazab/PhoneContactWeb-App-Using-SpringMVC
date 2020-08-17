bf262f128ae32cc051f7bd46c140ef0cfa2adf21
bassel <bassel.alazab@gmail.com>
2020-08-17 18:31:05 +0200

    get all abstracted method and define save/update/delete methods first.

--- src/main/java/com/basola/pcapp/service/ContactServiceImpl.java
+++ src/main/java/com/basola/pcapp/service/ContactServiceImpl.java
@@ -1,7 +1,51 @@
-
 package com.basola.pcapp.service;
 
+import com.basola.pcapp.dao.BaseDAO;
+import com.basola.pcapp.dao.ContactDAO;
+import com.basola.pcapp.domain.Contact;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class ContactServiceImpl {
+@Service
+public class ContactServiceImpl extends BaseDAO implements ContactService {
+    
+    @Autowired
+    private ContactDAO contactDAO;
+    
+    @Override
+    public void save(Contact c) {
+        contactDAO.save(c);
+    }
+    
+    @Override
+    public void update(Contact c) {
+        contactDAO.update(c);
+    }
+    
+    @Override
+    public void delete(Integer contactId) {
+        contactDAO.delete(contactId);
+    }
+    
+    @Override
+    public void delete(Integer[] contactId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public Contact findById(Integer contactId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public List<Contact> findUserContact(Integer userId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public List<Contact> findUserContact(Integer userId, String txt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
