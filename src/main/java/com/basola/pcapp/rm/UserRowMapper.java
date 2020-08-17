e808a9bb661f5328ab63f7e65bf90f73c9567dd0
bassel <bassel.alazab@gmail.com>
2020-08-17 14:41:15 +0200

    create method of MapRow that transfer data from mysql to object.

--- src/main/java/com/basola/pcapp/rm/UserRowMapper.java
+++ src/main/java/com/basola/pcapp/rm/UserRowMapper.java
@@ -1,7 +1,25 @@
-
 package com.basola.pcapp.rm;
 
+import com.basola.pcapp.domain.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
 
-public class UserRowMapper {
-    
+public class UserRowMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User u = new User();
+        u.setUserID(rs.getInt("userId"));
+        u.setName(rs.getString("name"));
+        u.setPhone(rs.getString("phone"));
+        u.setEmail(rs.getString("email"));
+        u.setAddress(rs.getString("address"));
+        u.setLoginName(rs.getString("loginName"));
+        u.setRole(rs.getInt("role"));
+        u.setLoginStatus(rs.getInt("loginStatus"));
+        
+        return u;
+    }
+
 }
