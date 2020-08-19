49133abe6019027b30537384f85a59bbc9bc2f39
bassel <bassel.alazab@gmail.com>
2020-08-19 11:28:06 +0200

    get info from FORM using spring model ui.

--- src/main/java/com/basola/pcapp/controller/UserController.java
+++ src/main/java/com/basola/pcapp/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.basola.pcapp.controller;
 
+import com.basola.pcapp.command.LoginCommand;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 
     @RequestMapping(value = {"/", "/index"})
-    public String index() {
+    public String index(Model m) {
+        m.addAttribute("command",new LoginCommand());
         return "index";
     }
 
