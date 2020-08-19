3e1ff8276b90c92b802c84233042937a7efe1a6e
bassel <bassel.alazab@gmail.com>
2020-08-19 12:05:42 +0200

    creates login operation :- first get data from model using spring ui to login command , second check to the login user role and status of account,

--- src/main/java/com/basola/pcapp/controller/UserController.java
+++ src/main/java/com/basola/pcapp/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.basola.pcapp.controller;
 
 import com.basola.pcapp.command.LoginCommand;
+import com.basola.pcapp.domain.User;
+import com.basola.pcapp.exception.UserBlockedException;
+import com.basola.pcapp.service.UserService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,11 +15,39 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
+    
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = {"/", "/index"})
     public String index(Model m) {
-        m.addAttribute("command",new LoginCommand());
+        m.addAttribute("command", new LoginCommand());
         return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String handleLogin(@ModelAttribute("command") LoginCommand cmd, Model m) {
+        try {
+            User loggedInUser = userService.login(cmd.getLoginName(), cmd.getPassword());
+            if (loggedInUser == null) {
+                //Failed
+                m.addAttribute("err", "Login Failed, Please enter correct user name and password");
+                return "index";
+            } else {
+                if (loggedInUser.getRole().equals(UserService.ROLE_ADMIN)) {
+                    return "redirect:admin/dashboard";
+                } else if (loggedInUser.getRole().equals(UserService.ROLE_USER)) {
+                    return "redirect:user/dashboard";
+                } else {
+                    m.addAttribute("err", "Invalid User Role");
+                    return "index";
+                }
+            }
+        } catch (UserBlockedException ex) {
+            m.addAttribute("err", ex.getMessage());
+            return "index";
+        }
+
     }
 
     @RequestMapping(value = "/user/dashboard")
@@ -25,5 +59,5 @@ public class UserController {
     public String adminDashboard() {
         return "dashboard_admin";
     }
-    
+
 }
