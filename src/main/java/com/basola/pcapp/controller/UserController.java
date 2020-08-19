1692d1473875d470fce10eccf7dbc552108380b5
bassel <bassel.alazab@gmail.com>
2020-08-19 10:41:06 +0200

    add / to get user and admin pages.

--- src/main/java/com/basola/pcapp/controller/UserController.java
+++ src/main/java/com/basola/pcapp/controller/UserController.java
@@ -11,12 +11,12 @@ public class UserController {
         return "index";
     }
 
-    @RequestMapping(value = "user/dashboard")
+    @RequestMapping(value = "/user/dashboard")
     public String userDashboard() {
         return "dashboard_user";
     }
 
-    @RequestMapping(value = "admin/dashboard")
+    @RequestMapping(value = "/admin/dashboard")
     public String adminDashboard() {
         return "dashboard_admin";
     }
