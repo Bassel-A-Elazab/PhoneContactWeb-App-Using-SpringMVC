bf0ea750b7618e29d64910980d292b53e8e890eb
bassel <bassel.alazab@gmail.com>
2020-08-19 11:16:08 +0200

    get and set login info.

--- src/main/java/com/basola/pcapp/command/LoginCommand.java
+++ src/main/java/com/basola/pcapp/command/LoginCommand.java
@@ -2,5 +2,24 @@
 package com.basola.pcapp.command;
 
 public class LoginCommand {
+    private String loginName;
+    private String password;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
 }
