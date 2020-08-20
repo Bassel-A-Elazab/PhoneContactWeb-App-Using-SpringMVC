afa4aa764032aaa1f0952d04a19be9800fdfeac9
bassel <bassel.alazab@gmail.com>
2020-08-20 05:10:01 +0200

    creates user command for handle data from jsp and front controller.

---
+++ src/main/java/com/basola/pcapp/command/UserCommand.java
@@ -0,0 +1,17 @@
+package com.basola.pcapp.command;
+
+import com.basola.pcapp.domain.User;
+
+public class UserCommand {
+
+    User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
