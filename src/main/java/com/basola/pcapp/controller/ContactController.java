f501b7694c38db275d472ba7044b04ea21a86e6f
bassel <bassel.alazab@gmail.com>
2020-08-20 06:01:56 +0200

    handle request of adding new contact and binding info to command.

--- src/main/java/com/basola/pcapp/controller/ContactController.java
+++ src/main/java/com/basola/pcapp/controller/ContactController.java
@@ -1,7 +1,25 @@
 
 package com.basola.pcapp.controller;
 
+import com.basola.pcapp.domain.Contact;
+import com.basola.pcapp.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
 public class ContactController {
+    
+    @Autowired
+    private ContactService contactService;
+    
+    @RequestMapping(value="/user/contact_form")
+    public String contactForm(Model m){
+        Contact contact = new Contact();
+        m.addAttribute("command",contact);
+        return "contact_form";
+    }
     
 }
