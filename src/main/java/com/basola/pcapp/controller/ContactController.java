446e13c9491a3440aee75364047b29433663857d
bassel <bassel.alazab@gmail.com>
2020-08-20 06:07:00 +0200

    handle action request of add new contact and save it.

--- src/main/java/com/basola/pcapp/controller/ContactController.java
+++ src/main/java/com/basola/pcapp/controller/ContactController.java
@@ -3,9 +3,11 @@ package com.basola.pcapp.controller;
 
 import com.basola.pcapp.domain.Contact;
 import com.basola.pcapp.service.ContactService;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -22,4 +24,17 @@ public class ContactController {
         return "contact_form";
     }
     
+    @RequestMapping(value="/user/savve_contact")
+    public String saveContact(@ModelAttribute("command") Contact c, Model m, HttpSession session){
+        try{
+            Integer userId = (Integer) session.getAttribute("userId");
+            c.setUserID(userId);    //FK
+            contactService.save(c);
+            return "redirect:clist?act=sv";
+        }catch(Exception ex){
+            ex.printStackTrace();
+            m.addAttribute("err","Failed To Save Contact");
+            return "contact_form";
+        }
+    }
 }
