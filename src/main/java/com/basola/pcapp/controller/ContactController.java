9a0aa16e035c4f061346673b9add0a6b50874518
bassel <bassel.alazab@gmail.com>
2020-08-20 07:44:21 +0200

    handle delete request for contact.

--- src/main/java/com/basola/pcapp/controller/ContactController.java
+++ src/main/java/com/basola/pcapp/controller/ContactController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -43,5 +44,11 @@ public class ContactController {
         Integer userId = (Integer) session.getAttribute("userId");
         m.addAttribute("contactList", contactService.findUserContact(userId));
         return "clist";
+    }
+    
+    @RequestMapping(value="/user/del_contact")
+    public String deleteContaact(@RequestParam("cid") Integer contactId){
+        contactService.delete(contactId);
+        return "redirect:clist?act=del";
     }
 }
