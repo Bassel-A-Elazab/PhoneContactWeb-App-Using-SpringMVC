0d5203d3837ece2b8a212c390d399bc1efd06920
bassel <bassel.alazab@gmail.com>
2020-08-23 17:29:51 +0200

    select all user name from sql to look if new user choose already token loginName or not.

--- src/main/java/com/basola/pcapp/service/UserServiceImpl.java
+++ src/main/java/com/basola/pcapp/service/UserServiceImpl.java
@@ -1,4 +1,3 @@
-
 package com.basola.pcapp.service;
 
 import com.basola.pcapp.dao.BaseDAO;
@@ -14,34 +13,34 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl extends BaseDAO implements UserService{
+public class UserServiceImpl extends BaseDAO implements UserService {
 
     @Autowired
     private UserDAO userDAO;
-    
+
     @Override
     public void register(User u) {
-        
+
         userDAO.save(u);
-        
+
     }
 
     @Override
     public User login(String loginName, String password) throws UserBlockedException {
         String sql = "SELECT userID, name, phone,email, address, role, loginStatus,loginName"
-                +" FROM user WHERE loginName=:ln AND password=:pw";
-        
+                + " FROM user WHERE loginName=:ln AND password=:pw";
+
         Map m = new HashMap();
-        m.put("ln",loginName);
-        m.put("pw",password);
-        try{
+        m.put("ln", loginName);
+        m.put("pw", password);
+        try {
             User u = getNamedParameterJdbcTemplate().queryForObject(sql, m, new UserRowMapper());
-            if(u.getLoginStatus().equals(UserService.LOGIN_STATUS_BLOCKED)){
+            if (u.getLoginStatus().equals(UserService.LOGIN_STATUS_BLOCKED)) {
                 throw new UserBlockedException("Your account has been blocked. Contact to admin");
-            }else{
+            } else {
                 return u;
             }
-        }catch(EmptyResultDataAccessException ex){
+        } catch (EmptyResultDataAccessException ex) {
             return null;
         }
     }
@@ -55,14 +54,20 @@ public class UserServiceImpl extends BaseDAO implements UserService{
     public void changeLoginStatus(Integer userId, Integer loginStatus) {
         String sql = "UPDATE user SET loginStatus=:lst WHERE userID=:uid";
         Map m = new HashMap();
-        m.put("uid",userId);
-        m.put("lst",loginStatus);
+        m.put("uid", userId);
+        m.put("lst", loginStatus);
         getNamedParameterJdbcTemplate().update(sql, m);
     }
 
     @Override
     public Boolean isUserNameExist(String username) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT count(loginName) FROM user WHERE loginName=?";
+        Integer count = getJdbcTemplate().queryForObject(sql, new String[]{username}, Integer.class);
+        if (count == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    
+
 }
